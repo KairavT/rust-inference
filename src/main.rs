3b23afb887a00018507656c8d0ce0ab4b63bf4b9
Kairav <kairavtupil@gmail.com>
2026-08-02 01:25:44 -0400

    load weights.json into weights struct in rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct Weights{
+    hidden_weights: Vec<Vec<f64>>,
+    hidden_bias: Vec<f64>,
+    output_weights: Vec<Vec<f64>>,
+    output_bias: Vec<f64>
 }
+
+fn main() {
+    let weights_json = std::fs::read_to_string("weights.json")
+    .expect("could not read file");
+
+    let weights: Weights = serde_json::from_str(&weights_json)
+    .expect("could not parse file");
+
+    println!("{:?}", weights.hidden_bias);
+} 
