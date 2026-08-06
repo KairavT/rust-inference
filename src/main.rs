9a681ad9ac4f29b37c96cb3c8e9c17660fc38cae
Kairav <kairavtupil@gmail.com>
2026-08-05 23:17:24 -0400

    moved forward pass into predict function

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,13 @@ struct Weights{
     output_bias: Vec<f64>
 }
 
+fn predict(input: &Vec<f64>, weights: &Weights) -> usize{
+    let hidden_test = hidden_layer(input, &weights.hidden_weights, &weights.hidden_bias, true);
+    let logits = hidden_layer(&hidden_test, &weights.output_weights, &weights.output_bias, false);
+    let argmax_result = argmax(&logits);
+    argmax_result
+}
+
 fn hidden_layer(input: &Vec<f64>, weights: &Vec<Vec<f64>>, bias: &Vec<f64>, apply_relu: bool) -> Vec<f64>{
     let mut outputs: Vec<f64> = Vec::new();
     for i in 0..weights.len(){
@@ -41,11 +48,7 @@ fn main() {
 
     let weights: Weights = serde_json::from_str(&weights_json)
     .expect("could not parse file");
-
-    let inputs_test = vec![4.900000095367432, 3.0, 1.399999976158142, 0.20000000298023224];
-    let hidden_test = hidden_layer(&inputs_test, &weights.hidden_weights, &weights.hidden_bias, true);
-    let logits = hidden_layer(&hidden_test, &weights.output_weights, &weights.output_bias, false);
-    println!("{:?}", logits);
-    let argmax_result = argmax(&logits);
-    println!("{}", argmax_result);
+    
+    let predict_test = predict(&vec![4.900000095367432, 3.0, 1.399999976158142, 0.20000000298023224], &weights);
+    println!("{}", predict_test);
 } 
