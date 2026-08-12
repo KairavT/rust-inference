d267db6a9fcbb62877e0d2ab7651f1d4ad72f8f0
Kairav <kairavtupil@gmail.com>
2026-08-11 22:04:34 -0400

    add train test split and accuracy eval

--- train.py
+++ train.py
@@ -2,11 +2,19 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
 import json
 
 X, y = load_iris(return_X_y=True)
-X = torch.tensor(X, dtype=torch.float32)
-y = torch.tensor(y, dtype=torch.long)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, stratify=y, random_state=123
+)
+X_train = torch.tensor(X_train, dtype=torch.float32)
+X_test = torch.tensor(X_test, dtype=torch.float32)
+
+y_train = torch.tensor(y_train, dtype=torch.long)
+y_test = torch.tensor(y_test, dtype=torch.long)
+
 
 model = nn.Sequential(
     nn.Linear(in_features=4, out_features=8),
@@ -17,8 +25,8 @@ model = nn.Sequential(
 optimizer = optim.Adam(model.parameters(), lr = 5e-3)
 loss_fn = nn.CrossEntropyLoss()
 for i in range(1001):
-    out = model(X)
-    loss = loss_fn(out, y)
+    out = model(X_train)
+    loss = loss_fn(out, y_train)
     loss.backward()
     optimizer.step()
     optimizer.zero_grad()
@@ -32,16 +40,29 @@ weights_and_biases = {
     "output_bias":model[2].bias.detach().numpy().tolist()
 }
 
+
+
 with open('weights.json', 'w') as f:
     json.dump(weights_and_biases, f)
 
 with torch.no_grad():
-    values_outputs = model(X)
+    values_outputs = model(X_test)
+
+with torch.no_grad():
+    train_preds = model(X_train).argmax(dim=1)
+    train_acc = (train_preds == y_train).float().mean().item()
+
+    test_preds = model(X_test).argmax(dim=1)
+    test_acc = (test_preds == y_test).float().mean().item()
+
+print(f'train accuracy: {train_acc} \
+      test accuracy: {test_acc}')
+      
 
 outputs_preds = values_outputs.argmax(dim=1)
 
 values_to_preds = {
-    "inputs":X.tolist(),
+    "inputs":X_test.tolist(),
     "predictions": outputs_preds.tolist()
 }
 
